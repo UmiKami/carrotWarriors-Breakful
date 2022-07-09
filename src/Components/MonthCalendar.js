b2ff0ce9e7b204f3eedd10048027950b371945e2
umikami <ggoku02@gmail.com>
2022-07-08 21:57:16 -0400

    month calendar styling 90% done

--- src/Components/MonthCalendar.js
+++ src/Components/MonthCalendar.js
@@ -5,25 +5,56 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { calendarActions } from "../store/calendar";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    text: {
+      primary:'#436E70',
+      secondary: '#B5BEC6',
+      disabled: '#D0EDED'
+    },
+    primary: {
+      main: '#436E70',
+      light: '#629EA0'
+    },
+    action:{
+      active: '#436E70',
+      selected:'#436E70',
+      focus: '#436E70'
+    }
+  },
+  typography: {
+    fontFamily: 'Inter',
+    fontSize: 18,
+    button:{
+      fontWeight: "600 !important"
+    }
+  }
+})
 
 const MonthCalendar = () => {
   const [calendarVal, setCalendarVal] = useState({});
   const dispatch = useDispatch();
-	dispatch(calendarActions.setSelectedDate(calendarVal))
+  dispatch(calendarActions.setSelectedDate(calendarVal))
 
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <StaticDatePicker
-          label="Date"
-          showToolbar={false}
-          value={calendarVal}
-          onChange={(val) => {
-            setCalendarVal(val);
-          }}
-          disablePast={true}
-          renderInput={(params) => <TextField {...params} />}
-        />
+        <ThemeProvider theme={theme}>
+          <StaticDatePicker
+            className="calendar"
+            label="Date"
+            showToolbar={false}
+            value={calendarVal}
+            onChange={(val) => {
+              setCalendarVal(val);
+            }}
+            disablePast={true}
+            disableMaskedInput={true}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </ThemeProvider>
       </LocalizationProvider>
     </>
   );
--- src/Views/Dashboard.js
+++ src/Views/Dashboard.js
@@ -3,14 +3,22 @@ import { useSelector } from "react-redux";
 
 const Dashboard = () => {
   const selectedDate = useSelector((state) => state.calendar.selectedDate);
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
   console.log(selectedDate);
 
   return (
     <>
-      <h1>You are in the dashboard!!</h1>
-      <div className="calendar-wrapper">
-			  <MonthCalendar/>
-      </div>
+    {
+      isLoggedIn ? (
+          <>
+            <h1>You are in the dashboard!!</h1>
+            <div className="calendar-wrapper">
+		          <MonthCalendar/>
+            </div>
+          </>
+      ) : (<h1>You are not logged in</h1>)
+    }
+    
     </>
   );
 };
