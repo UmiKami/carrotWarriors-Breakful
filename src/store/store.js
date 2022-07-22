b7ef61a04cfd10fbf26a5ca2b4fcc5b71657c23a
umikami <ggoku02@gmail.com>
2022-07-21 22:14:07 -0400

    added dashboard slice to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth";
 import calendarSlice from "./calendar";
+import dashboardSlice from "./dashboard";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     calendar: calendarSlice.reducer,
+    dashboard: dashboardSlice.reducer,
   }
 });
 export default store;
