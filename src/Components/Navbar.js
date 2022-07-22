fddaec2de2e5b79926cb562cc94d2c633c3c7259
umikami <ggoku02@gmail.com>
2022-07-22 12:15:15 -0400

    added navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+        <div className="nav__profile">
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
