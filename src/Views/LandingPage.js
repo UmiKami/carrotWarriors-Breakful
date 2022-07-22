832199197e02c63e0d3d6e3324c334c44d267ee1
umikami <ggoku02@gmail.com>
2022-07-21 22:16:07 -0400

    repositioned login button

--- src/Views/LandingPage.js
+++ src/Views/LandingPage.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import LPimage from "../assets/LPimage.png";
@@ -11,35 +12,41 @@ const LandingPage = () => {
         dispatch(authActions.signup())
     }
 
+    useEffect(()=>{
+      isLoggedIn ? navigate('/dashboard') : navigate("/")
+    },[isLoggedIn])
+
+    console.log(isLoggedIn);
+
     return (
-        <main id="content-wrapper"> 
+      <main id="content-wrapper">
+        <div className="main-container">
+          <div className="image-container">
+            <img className="main-image" src={LPimage} alt="blablabla" />
+          </div>
 
-            <div className="main-container">
+          <div className="text-container">
+            <h1>Take Breakful Breaks!</h1>
 
-                <div className="image-container">
-                    <img className="main-image" src={LPimage} alt="blablabla" />
-                </div>
+            <ul>
+              <li>A system that makes taking breaks an easy and fun task</li>
+              <li>Easily schedule breaks based on your Google Calendar</li>
+              <li>Feel free to choose your breaks duration and content</li>
+            </ul>
 
-                <div className="text-container">
-                    <h1>Take Breakful Breaks!</h1>
-
-                    <ul>
-                        <li>A system that makes taking breaks an easy and fun task</li>
-                        <li>Easily schedule breaks based on your Google Calendar</li>
-                        <li>Feel free to choose your breaks duration and content</li>
-                    </ul>
-                    
-
-                </div>
-
-                <div className="sign-up-container">
-                    {!isLoggedIn ? <button onClick={handleSignUp}>Sign up with Google Account</button> : navigate(`/dashboard`)}
-                </div>
-
+            <div className="sign-up-container">
+              {!isLoggedIn ? (
+                <button onClick={handleSignUp}>
+                  Sign up with Google Account
+                </button>
+              ) : (
+                ""
+              )}
             </div>
-            
-        </main>
-    )
+          </div>
+        </div>
+      </main>
+    );
 }
 
 export default LandingPage; 
