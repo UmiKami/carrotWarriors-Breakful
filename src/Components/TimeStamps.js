c9b036fedb3c27826982cc517cd029d9806c5e03
umikami <ggoku02@gmail.com>
2022-07-21 22:12:33 -0400

    enchanced timestamps style, added custom navbar

--- src/Components/TimeStamps.js
+++ src/Components/TimeStamps.js
@@ -1,32 +1,38 @@
-import React, { Component }  from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { dashboardActions } from "../store/dashboard";
 
 const TimeStamps = () => {
-    return (
+  const dispatch = useDispatch();
+
+  const confirmDateTime = () => {
+    dispatch(dashboardActions.setDateTimeConfirmed(true))
+  }
+
+  return (
     <>
-        
-        <div className='timeSlots'>
-            <h1>Time</h1>
-
-            <button>8:00 AM</button>
-            <button>8:30 AM</button>
-            <button>10:00 AM</button>
-            <button>12:00 PM</button>
-            <button>12:30 PM</button>
-            <button>1:00 PM</button>
-            <button>3:00 PM</button>
-            <button>3:30 PM</button>
-            <button>4:00 PM</button>
-            <button>4:30 PM</button>
-            <button>6:30 PM</button>
-            <button>8:00 PM</button>
-            <button>9:30 PM</button>
-            <button>10:30 PM</button>
-
-        </div>  
-
+      <div className="timeSlots">
+        <h1 className="dateTime-header">Time</h1>
+        <div className="timeSlots__buttons">
+          <button>8:00 AM</button>
+          <button>8:30 AM</button>
+          <button>10:00 AM</button>
+          <button>12:00 PM</button>
+          <button>12:30 PM</button>
+          <button>1:00 PM</button>
+          <button>3:00 PM</button>
+          <button>3:30 PM</button>
+          <button>4:00 PM</button>
+          <button>4:30 PM</button>
+          <button>6:30 PM</button>
+          <button>8:00 PM</button>
+          <button>9:30 PM</button>
+          <button>10:30 PM</button>
+        </div>
+        <button className="confirm" onClick={confirmDateTime}>Confirm</button>
+      </div>
     </>
-
-    )};
+  );
+};
 
 export default TimeStamps;
