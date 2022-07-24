20bd533f2eadd35e6b0d4595dfe7d00a2a2b3ee4
umikami <ggoku02@gmail.com>
2022-07-24 00:19:55 -0400

    added nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
-function Navbar() {
-  return (
-    <nav className='nav'>
-        <div className="nav__profile">
+const Nav = () => {
+  // if url is a local folder use the format below
+  // const imageUrl = require("../assets/<name of image with format>");  
 
-        </div>
-    </nav>
-  )
-}
+  // if the url is from a site or http request use this format below
+  const imageUrl = "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80";  
 
-export default Navbar
+    return (
+        <>
+            <Navbar className="nav">
+                <Container fluid className="d-flex justify-content-end">
+                    <Navbar.Brand href="#home">
+                        <div
+                            className="nav__profile"
+                            style={{
+                                backgroundImage: `url("${imageUrl}")`
+                            }}
+                        ></div>
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Nav;
