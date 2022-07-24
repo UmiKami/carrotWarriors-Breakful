268620accb808d03bfbb2635ea5f6b4bd55c374c
umikami <ggoku02@gmail.com>
2022-07-23 23:49:47 -0400

    added day format, started working on redux for break type

--- src/Components/BreakOptions.js
+++ src/Components/BreakOptions.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { dashboardActions } from "../store/dashboard";
 
 const BreakOptions = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch("");
+    const [customType, setCustomType] = useState()
 
     const returnToDateTime = () => {
         dispatch(dashboardActions.setDateTimeConfirmed(false))
@@ -13,11 +14,24 @@ const BreakOptions = () => {
         dispatch(dashboardActions.setTypeDurationConfirmed(true))
     }
 
+    const selectedTime = useSelector(state=>state.calendar.selectedTime)
+    // converts 24 hour format to 12 hour format
+    const formattedTime =
+        parseInt(selectedTime.slice(0, 2)) >= 12
+            ? (parseInt(selectedTime.slice(0, 2)) - 12).toString() +
+              selectedTime.slice(-3) +
+              " PM"
+            : selectedTime + " AM";
+
+    const setType = (type) => {
+        
+    }
+
     return (
         <section className="breakOpt">
             <div className="breakOpt__typeDuration">
                 <h1 className="breakOpt__header">
-                    You are scheduling a break at 10:00 AM !
+                    You are scheduling a break at {formattedTime} !
                 </h1>
                 {/* Type */}
                 <div className="breakOpt__wrapper">
@@ -25,20 +39,46 @@ const BreakOptions = () => {
                         <h1 className="breakOpt__typeDuration-typeHeader">
                             What would you like to do during your break?
                         </h1>
-                        <button className="breakOpt__typeDuration-typeBtn">
+                        <button
+                            className="breakOpt__typeDuration-typeBtn"
+                            onClick={() => setType("Water")}
+                        >
                             Water time
                         </button>
-                        <button className="breakOpt__typeDuration-typeBtn">
+                        <button
+                            className="breakOpt__typeDuration-typeBtn"
+                            onClick={() => setType("Coffee")}
+                        >
                             Coffee Time
                         </button>
-                        <button className="breakOpt__typeDuration-typeBtn">
+                        <button
+                            className="breakOpt__typeDuration-typeBtn"
+                            onClick={() => setType("Stretching")}
+                        >
                             Stretching time
                         </button>
-                        <button className="breakOpt__typeDuration-typeBtn">
+                        <button
+                            className="breakOpt__typeDuration-typeBtn"
+                            onClick={() => setType("Nap")}
+                        >
                             Nap time
                         </button>
-                        <button className="breakOpt__typeDuration-typeBtn">
-                            <span>Customize your break</span> time
+                        <button
+                            className="breakOpt__typeDuration-typeBtn"
+                            onClick={() => setType(customType)}
+                        >
+                            <span
+                                className="breakOpt__typeDuration-customType"
+                                contentEditable
+                                onInput={(inpt) =>
+                                    setCustomType(
+                                        inpt.currentTarget.textContent
+                                    )
+                                }
+                            >
+                                Customize your break
+                            </span>{" "}
+                            time
                         </button>
                     </div>
                     {/* Time */}
@@ -59,14 +99,28 @@ const BreakOptions = () => {
                             10 mins
                         </button>
                         <button className="breakOpt__typeDuration-timeBtn">
-                            <span contentEditable className="breakOpt__typeDuration-customTime"></span> mins
+                            <span
+                                contentEditable
+                                className="breakOpt__typeDuration-customTime"
+                            ></span>{" "}
+                            mins
                         </button>
                     </div>
                 </div>
 
                 <div className="breakOpt__actionBtn">
-                    <button className="breakOpt__returnBtn" onClick={returnToDateTime}>Return</button>
-                    <button className="breakOpt__confirmBtn" onClick={confirmBreak}>Confirm</button>
+                    <button
+                        className="breakOpt__returnBtn"
+                        onClick={returnToDateTime}
+                    >
+                        Return
+                    </button>
+                    <button
+                        className="breakOpt__confirmBtn"
+                        onClick={confirmBreak}
+                    >
+                        Confirm
+                    </button>
                 </div>
             </div>
         </section>
