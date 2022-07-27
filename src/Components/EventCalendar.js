24a2a1d079d7439a19fb973cad7e53863f1cda08
umikami <ggoku02@gmail.com>
2022-07-27 13:01:38 -0400

    added scrollbar to event calendar and enhanced props obtention

--- src/Components/EventCalendar.js
+++ src/Components/EventCalendar.js
@@ -1,40 +1,58 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-const EventCalendar = (props) => {
+const EventCalendar = ({rowHour}) => {
   const events = useSelector(state => state.calendar.events)
 
   return (
-    <>
-      <section className="eventCal__timeStamp">
-        <span className="eventCal__timeStamp-timeString">{props.rowHour}h</span>
-        <span className="eventCal__timeStamp-timeOutline"></span>
-        {(() => {
-          let eventsAtThisHour = []
+      <>
+          <section className="eventCal__timeStamp" >
+              <span className="eventCal__timeStamp-timeString">
+                  {rowHour}h
+              </span>
+              <span className="eventCal__timeStamp-timeOutline"></span>
+              {(() => {
+                  let eventsAtThisHour = [];
 
-          if(events.payload === undefined) {
-            return;
-          }
+                  if (events.payload === undefined) {
+                      return;
+                  }
 
-          for (const event of events.payload) {
-            const eventStartHour = new Date(event.start.dateTime).getHours();
+                  for (const event of events.payload) {
+                      const eventStartHour = new Date(
+                          event.start.dateTime
+                      ).getHours();
 
-            if(eventStartHour === props.rowHour) {
-                eventsAtThisHour.push(
-                <span className="eventCal__timeStamp-event">
-                  <p className="eventCal__timeStamp-eventHeader">
-                    {event.summary}
-                  </p>
-                  <p className="eventCal__timeStamp-eventTime">{new Date(event.start.dateTime).toLocaleString('en-US', { hour: 'numeric', hour12: true })} - {new Date(event.end.dateTime).toLocaleString('en-US', { hour: 'numeric', hour12: true })}</p>
-                </span>
-              )
-            }
-          }
-          
-          return eventsAtThisHour;
-        })()}
-      </section>
-    </>
+                      if (eventStartHour === rowHour) {
+                          eventsAtThisHour.push(
+                              <span className="eventCal__timeStamp-event">
+                                  <p className="eventCal__timeStamp-eventHeader">
+                                      {event.summary}
+                                  </p>
+                                  <p className="eventCal__timeStamp-eventTime">
+                                      {new Date(
+                                          event.start.dateTime
+                                      ).toLocaleString("en-US", {
+                                          hour: "numeric",
+                                          hour12: true,
+                                      })}{" "}
+                                      -{" "}
+                                      {new Date(
+                                          event.end.dateTime
+                                      ).toLocaleString("en-US", {
+                                          hour: "numeric",
+                                          hour12: true,
+                                      })}
+                                  </p>
+                              </span>
+                          );
+                      }
+                  }
+
+                  return eventsAtThisHour;
+              })()}
+          </section>
+      </>
   );
 };
 
