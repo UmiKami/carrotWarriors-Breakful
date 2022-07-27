99d5b952903991031ebbf2bc7f7f02a0150155eb
umikami <ggoku02@gmail.com>
2022-07-27 15:44:34 -0400

    Changed format of time labels

--- src/Components/EventCalendar.js
+++ src/Components/EventCalendar.js
@@ -1,59 +1,63 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-const EventCalendar = ({rowHour}) => {
-  const events = useSelector(state => state.calendar.events)
+const EventCalendar = ({ rowHour }) => {
+    const events = useSelector((state) => state.calendar.events);
+    const formattedTime =
+        // 10:05
+        rowHour >= 12
+            ? (rowHour != 12 ? rowHour - 12 : rowHour).toString() + " PM"
+            : rowHour.toString() + " AM";
+    return (
+        <>
+            <section className="eventCal__timeStamp">
+                <span className="eventCal__timeStamp-timeString">
+                    {formattedTime}
+                </span>
+                <span className="eventCal__timeStamp-timeOutline"></span>
+                {(() => {
+                    let eventsAtThisHour = [];
 
-  return (
-      <>
-          <section className="eventCal__timeStamp" >
-              <span className="eventCal__timeStamp-timeString">
-                  {rowHour}h
-              </span>
-              <span className="eventCal__timeStamp-timeOutline"></span>
-              {(() => {
-                  let eventsAtThisHour = [];
+                    if (events.payload === undefined) {
+                        return;
+                    }
 
-                  if (events.payload === undefined) {
-                      return;
-                  }
+                    for (const event of events.payload) {
+                        const eventStartHour = new Date(
+                            event.start.dateTime
+                        ).getHours();
 
-                  for (const event of events.payload) {
-                      const eventStartHour = new Date(
-                          event.start.dateTime
-                      ).getHours();
+                        if (eventStartHour === rowHour) {
+                            eventsAtThisHour.push(
+                                <span className="eventCal__timeStamp-event">
+                                    <p className="eventCal__timeStamp-eventHeader">
+                                        {event.summary}
+                                    </p>
+                                    <p className="eventCal__timeStamp-eventTime">
+                                        {new Date(
+                                            event.start.dateTime
+                                        ).toLocaleString("en-US", {
+                                            hour: "numeric",
+                                            hour12: true,
+                                        })}{" "}
+                                        -{" "}
+                                        {new Date(
+                                            event.end.dateTime
+                                        ).toLocaleString("en-US", {
+                                            hour: "numeric",
+                                            hour12: true,
+                                        })}
+                                    </p>
+                                </span>
+                            );
+                        }
+                    }
 
-                      if (eventStartHour === rowHour) {
-                          eventsAtThisHour.push(
-                              <span className="eventCal__timeStamp-event">
-                                  <p className="eventCal__timeStamp-eventHeader">
-                                      {event.summary}
-                                  </p>
-                                  <p className="eventCal__timeStamp-eventTime">
-                                      {new Date(
-                                          event.start.dateTime
-                                      ).toLocaleString("en-US", {
-                                          hour: "numeric",
-                                          hour12: true,
-                                      })}{" "}
-                                      -{" "}
-                                      {new Date(
-                                          event.end.dateTime
-                                      ).toLocaleString("en-US", {
-                                          hour: "numeric",
-                                          hour12: true,
-                                      })}
-                                  </p>
-                              </span>
-                          );
-                      }
-                  }
-
-                  return eventsAtThisHour;
-              })()}
-          </section>
-      </>
-  );
+                    return eventsAtThisHour;
+                })()}
+            </section>
+        </>
+    );
 };
 
 export default EventCalendar;
