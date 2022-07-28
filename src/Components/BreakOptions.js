d336a01d916569ba46c92a96c66768fab7b77836
Beatriz <becarolf@gmail.com>
2022-07-27 23:41:49 -0400

    implemented of event creation on google calendar

--- src/Components/BreakOptions.js
+++ src/Components/BreakOptions.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { breakActions } from "../store/break";
 import { dashboardActions } from "../store/dashboard";
+import { calendarActions } from "../store/calendar";
 
 const BreakOptions = () => {
     const dispatch = useDispatch("");
@@ -11,24 +12,90 @@ const BreakOptions = () => {
         dispatch(dashboardActions.setDateTimeConfirmed(false))
     }
 
+    const selectedTime = useSelector(state=>state.calendar.selectedTime)
+    const selectedDate = useSelector((state) => state.calendar.selectedDate);
+    const breakType = useSelector((state) => state.break.breakType);
+    const duration = useSelector((state) => state.break.breakDuration);
+
+    const createEvent = () => {
+        const startDate = new Date(selectedDate.year, selectedDate.month, selectedDate.day.date).toLocaleDateString('en-CA')
+
+        requestGoogleCalendarApi('/calendars/primary/events',
+        {
+            start: {
+                dateTime: `${startDate}T${selectedTime}:00`,
+                timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+            },
+            end: {
+                dateTime: `${startDate}T${calcEndTime()}:00`,
+                timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+            },
+            summary: breakType
+
+        });
+    }
+
+    const buildAuthorizationHeaders = () => {
+        return {
+            "Authorization": `Bearer ${localStorage.getItem('access_token')}`
+        }
+    }
+
+    // TODO: remember to reuse the function
+    const requestGoogleCalendarApi = (url, body) => {
+        const googleCalendarEndpoint = 'https://www.googleapis.com/calendar/v3';
+    
+        const fullUrl = `${googleCalendarEndpoint}${url}`
+        
+        fetch(fullUrl, {
+            method: 'POST',
+            headers: buildAuthorizationHeaders(),
+            body: JSON.stringify(body)
+        })
+        .then(response => response.json())
+    }
+
+    // TODO: remember to reuse the function
+    const calcEndTime = () => {
+        let endTime = "";
+
+        if (
+            parseInt(selectedTime.slice(-2)) == 30 &&
+            parseInt(duration.slice(0, 2)) == 30
+        ) {
+            endTime = (parseInt(selectedTime.slice(-2)) + 1).toString() + ": 00";
+        } else {
+            endTime =
+                selectedTime.slice(0, 2) +
+                ":" +
+                (
+                    parseInt(selectedTime.slice(-2)) +
+                    parseInt(duration.slice(0, 2))
+                ).toString();
+        }
+
+        return endTime;
+    };
+
     const confirmBreak = () => {
         // create event on google calendar
+        createEvent();
 
 
         dispatch(dashboardActions.setTypeDurationConfirmed(true))
     }
 
-    const selectedTime = useSelector(state=>state.calendar.selectedTime)
+
     // converts 24 hour format to 12 hour format
     const formattedTime =
         // 10:05
         parseInt(selectedTime.slice(0, 2)) >= 12
-            ? (parseInt(selectedTime.slice(0, 2)) != 12
-                  ? parseInt(selectedTime.slice(0, 2)) - 12
-                  : parseInt(selectedTime.slice(0, 2))
-              ).toString() +
-              selectedTime.slice(-3) +
-              " PM"
+            ? (parseInt(selectedTime.slice(0, 2)) !== 12
+                ? parseInt(selectedTime.slice(0, 2)) - 12
+                : parseInt(selectedTime.slice(0, 2))
+            ).toString() +
+            selectedTime.slice(-3) +
+            " PM"
             : selectedTime + " AM";
 
     const setType = (type) => {
